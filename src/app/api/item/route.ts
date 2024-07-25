404b5d4f0d392512d8ac77d95188e4edc648fa6a
John Roice Aldeza <jroicealdeza@gmail.com>
2024-07-25 19:42:21 +0800

    chore: Add route for retrieving a single item by slug

---
+++ src/app/api/item/route.ts
@@ -0,0 +1,26 @@
+import ItemListing from "@/types/item-listing";
+import { NextRequest, NextResponse } from "next/server";
+
+//get all the data of a single item
+export async function GET(
+  req: NextRequest
+): Promise<NextResponse<ItemListing | { error: string }>> {
+  const slug = req.nextUrl.searchParams.get("slug");
+  if (!slug) {
+    return NextResponse.json({ error: "Missing slug" }, { status: 400 });
+  }
+
+  const item = await fetch(
+    `${process.env.STRAPI_URL}/api/item-listings?populate=images&filters[slug][$eq]=${slug}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+      },
+    }
+  );
+  const data = await item.json();
+  if (data.data.length === 0) {
+    return NextResponse.json({ error: "Item not found" }, { status: 404 });
+  }
+  return NextResponse.json(data.data[0], { status: 200 });
+}
