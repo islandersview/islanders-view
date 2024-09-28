01d2251530bf8180b48b5ce6002d0f49b3be6387
John Roice Aldeza <jroicealdeza@gmail.com>
2024-09-28 19:01:45 +0800

    Refactor layout component: Comment out unused code and remove unnecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,21 +6,21 @@ import Navbar from "@/components/Navbar/Navbar";
 
 const font = Poppins({ weight: "400", subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    absolute: "Islander's View",
-    template: "%s | Islander's View",
-  },
-  // description: "Find your dream properties here in Davao City!",
-  // icons: ["/logo.png"],
-  // keywords: ["real estate", "properties", "vehicles", "Davao City", "Samal"],
-  // openGraph: {
-  //   type: "website",
-  //   locale: "en_US",
-  //   title: "Islander's View",
-  //   description: "Find your dream properties here in Davao City!",
-  // },
-};
+// export const metadata: Metadata = {
+//   title: {
+//     absolute: "Islander's View",
+//     template: "%s | Islander's View",
+//   },
+//   // description: "Find your dream properties here in Davao City!",
+//   // icons: ["/logo.png"],
+//   // keywords: ["real estate", "properties", "vehicles", "Davao City", "Samal"],
+//   // openGraph: {
+//   //   type: "website",
+//   //   locale: "en_US",
+//   //   title: "Islander's View",
+//   //   description: "Find your dream properties here in Davao City!",
+//   // },
+// };
 
 export default function RootLayout({
   children,
@@ -29,13 +29,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="my-theme">
-      <body className={font.className + " flex flex-col min-h-screen"}>
+      {/* <body className={font.className + " flex flex-col min-h-screen"}>
         <Navbar />
         <div>{children}</div>
         <div className="mt-auto">
           <Footer />
         </div>
-      </body>
+      </body> */}
     </html>
   );
 }
