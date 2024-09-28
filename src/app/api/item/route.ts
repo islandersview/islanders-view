83e5529f016c9e37e45c10de30b5c43d94a14e81
John Roice Aldeza <jroicealdeza@gmail.com>
2024-09-28 18:53:54 +0800

    Refactor item route: Comment out unused code and remove unnecessary imports

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -4,24 +4,24 @@ import { NextRequest, NextResponse } from "next/server";
 //get all the data of a single item
 export async function GET(
   req: NextRequest
-): Promise<NextResponse<ItemListing | { error: string }>> {
-  const slug = req.nextUrl.searchParams.get("slug");
-  if (!slug) {
-    return NextResponse.json({ error: "Missing slug" }, { status: 400 });
-  }
+) {
+  // const slug = req.nextUrl.searchParams.get("slug");
+  // if (!slug) {
+  //   return NextResponse.json({ error: "Missing slug" }, { status: 400 });
+  // }
 
-  const item = await fetch(
-    `${process.env.STRAPI_URL}/api/item-listings?populate=images&filters[slug][$eq]=${slug}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-      },
-    }
-  );
-  const data = await item.json();
+  // const item = await fetch(
+  //   `${process.env.STRAPI_URL}/api/item-listings?populate=images&filters[slug][$eq]=${slug}`,
+  //   {
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+  //     },
+  //   }
+  // );
+  // const data = await item.json();
 
-  if (!data || !data.data ||  data.data.length === 0) {
-    return NextResponse.json({ error: "Item not found" }, { status: 404 });
-  }
-  return NextResponse.json(data.data[0], { status: 200 });
+  // if (!data || !data.data ||  data.data.length === 0) {
+  //   return NextResponse.json({ error: "Item not found" }, { status: 404 });
+  // }
+  // return NextResponse.json(data.data[0], { status: 200 });
 }
